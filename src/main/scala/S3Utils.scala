0f7fff7af318bdbcb089a11cf18f85bb9b171f92
Rob MacDonald <rob@robmacdonald.com>
2024-10-30 23:13:02 +0000

    More s3 logging

--- src/main/scala/S3Utils.scala
+++ src/main/scala/S3Utils.scala
@@ -5,11 +5,13 @@ import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.{S3Client, S3ClientBuilder}
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import software.amazon.awssdk.http.apache.ApacheHttpClient
+import org.slf4j.LoggerFactory
 
 object S3Utils {
   private val accessKeyId = sys.env("AWS_ACCESS_KEY_ID")
   private val secretAccessKey = sys.env("AWS_SECRET_ACCESS_KEY")
   private val region = sys.env("AWS_REGION")
+  private val logger = LoggerFactory.getLogger(this.getClass)
 
   private val credentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey)
   private val client: S3Client = S3Client.builder()
@@ -19,12 +21,24 @@ object S3Utils {
     .build()
 
   def saveToS3(pdfBlob: Array[Byte], key: String): IO[Unit] = IO {
-    val request = PutObjectRequest.builder()
-      .bucket("docusign-pdfs-databonds")
-      .key(key)
-      .contentType("application/pdf")
-      .build()
+    logger.info(s"Starting S3 upload - bucket: docusign-pdfs-databonds, key: $key")
+    logger.info(s"Using AWS Region: $region")
+    
+    try {
+      val request = PutObjectRequest.builder()
+        .bucket("docusign-pdfs-databonds")
+        .key(key)
+        .contentType("application/pdf")
+        .build()
 
-    client.putObject(request, RequestBody.fromBytes(pdfBlob))
-  }
+      client.putObject(request, RequestBody.fromBytes(pdfBlob))
+      logger.info(s"Successfully uploaded to S3: $key")
+    } catch {
+      case e: Exception =>
+        logger.error(s"S3 upload failed with error: ${e.getMessage}")
+        logger.error(s"Full error details: ${e.toString}")
+        throw e
+    }
+}
+
 }
