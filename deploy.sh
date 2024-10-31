1ff4c7e8f19484499e767420a9bf6d9be0b72832
Rob MacDonald <rob@robmacdonald.com>
2024-10-31 22:19:10 +0000

    Update deploy script for GCC

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-
 echo "Building Docker image..."
-docker buildx build --platform linux/amd64 -t 868232756949.dkr.ecr.eu-west-2.amazonaws.com/docusign-ingester:latest .
+docker build --platform linux/amd64 -t europe-north1-docker.pkg.dev/brewing-436514/docusigningester/docusigningester:latest .
 
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 868232756949.dkr.ecr.eu-west-2.amazonaws.com
+echo "Pushing to Google Artifact Registry..."
+docker push europe-north1-docker.pkg.dev/brewing-436514/docusigningester/docusigningester:latest
 
-echo "Pushing to ECR..."
-docker push 868232756949.dkr.ecr.eu-west-2.amazonaws.com/docusign-ingester:latest
-
-echo "Deploying to ECS..."
-aws ecs update-service \
-  --cluster docusign-cluster \
-  --service docusign-service \
-  --task-definition docusign-ingester \
-  --force-new-deployment \
-  --network-configuration "awsvpcConfiguration={subnets=[subnet-0f797677],securityGroups=[sg-1efa1477]}" \
-  --region eu-west-2
-
+echo "Deploying to Google Cloud Run..."
+# aws ecs update-service \
+#   --cluster docusign-cluster \
+#   --network-mode awsvpc \
+#   --service docusign-service \
+#     --task-definition docusign-ingester \
+#   --force-new-deployment \
+#   --region eu-west-2 
 
 echo "Deployment complete! Changes will be live in a few moments."
+gcloud run deploy docusign-ingester --image europe-north1-docker.pkg.dev/brewing-436514/docusigningester/docusigningester:latest --platform managed --region europe-north1
