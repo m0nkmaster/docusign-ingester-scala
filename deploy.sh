064f32b12d1e2e76706a50f4612d621c29f4ee5a
Rob MacDonald <rob@robmacdonald.com>
2024-11-04 17:45:09 +0000

    Deploy file cleanup.

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,6 @@ echo "Pushing to Google Artifact Registry..."
 docker push europe-north1-docker.pkg.dev/brewing-436514/docusigningester/docusigningester:latest
 
 echo "Deploying to Google Cloud Run..."
-# aws ecs update-service \
-#   --cluster docusign-cluster \
-#   --network-mode awsvpc \
-#   --service docusign-service \
-#     --task-definition docusign-ingester \
-#   --force-new-deployment \
-#   --region eu-west-2 
 
 echo "Deployment complete! Changes will be live in a few moments."
 gcloud run deploy docusign-ingester --image europe-north1-docker.pkg.dev/brewing-436514/docusigningester/docusigningester:latest --platform managed --region europe-north1
