70fd16ca6558d3ec4e7e9a27160a17e255dce6cf
Rob MacDonald <rob@robmacdonald.com>
2024-11-04 17:45:35 +0000

    Remove unusded dep

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,11 +2,8 @@ import org.http4s.server.blaze._
 import cats.effect.{IO, IOApp}
 import org.http4s.blaze.server.BlazeServerBuilder
 import scala.concurrent.ExecutionContext.global
-// import io.github.cdimascio.dotenv.Dotenv
 
 object Main extends IOApp.Simple {
-  // Load environment variables from .env file
-  // val dotenv = Dotenv.load()
 
   def run: IO[Unit] = {
     // Initialize and start the HTTP server here
